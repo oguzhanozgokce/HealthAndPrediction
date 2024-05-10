1ec47251d7e6e4c7d887feab01fff78b23bd03e0
Oguzhan <ozgokceoguzhan34@gmail.com>
2024-05-10 21:51:12 +0300

    solutions for necessary warnings have been introduced

--- app/src/main/java/com/oguzhanozgokce/healthandprediction/common/Constants.kt
+++ app/src/main/java/com/oguzhanozgokce/healthandprediction/common/Constants.kt
@@ -2,6 +2,6 @@ package com.oguzhanozgokce.healthandprediction.common
 
 object Constants {
     const val BASE_URL_HEALTH = "https://api.collectapi.com/health/"
-    const val API_KEY_HEALTH = "YOUR_API_KEY"
+    const val API_KEY_HEALTH = "3vc1hah5KrdUO2JLVZ3xkD:2m2ptbeXwPFOK9pFDeDTO9"
     const val BASE_URL_NEWS = "https://api.collectapi.com/news/"
 }
